a482f54da192aba7ca9a886bbb0ffbe4083dfa0e
michifueby <michi.fueby@gmail.com>
2023-10-23 21:50:18 +0200

    Add comments to NodeJS app

--- Lab2/app/app.js
+++ Lab2/app/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// Create the app
 var app = express();
 
 // view engine setup
